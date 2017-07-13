44f34a4bcaba6cda45f7693d6f0db7345f4f2138
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-13 17:08:41 -0400

    menu bar setup!

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const electron = require('electron')
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
 });
+
+const menuTemplate = [
+  {
+    label: 'Click'
+  }
+];
